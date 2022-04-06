1705fac5d0d68b31857e28ec3899ee67f5d2d91c
VanderSVan <101255612+VanderSVan@users.noreply.github.com>
2022-04-06 11:21:14 +0300

    Add errors handler

--- api_report/db/create_db.py
+++ api_report/db/create_db.py
@@ -1,4 +1,5 @@
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from psycopg2 import DatabaseError
 from utils import PsqlDatabaseConnection
 from db_tools import Database
 from db_tools import DatabaseRole
@@ -8,26 +9,34 @@ from db_tools import DatabaseUser
 
 if __name__ == '__main__':
     # first connection via superuser
-    with PsqlDatabaseConnection(user="postgres",
-                                password="12345",
-                                host="127.0.0.1",
-                                port="5432",
-                                isolation_level=ISOLATION_LEVEL_AUTOCOMMIT) as db:
-        # Create new database:
-        new_db = Database('university', db.connection)
-        new_db.create_postgresql_db()
+    try:
+        with PsqlDatabaseConnection(dbname="postgres",
+                                    user="postgres",
+                                    password="12345",
+                                    host="127.0.0.1",
+                                    port="5432",
+                                    isolation_level=ISOLATION_LEVEL_AUTOCOMMIT) as db:
+            # Create new database:
+            new_db = Database('university', db.connection)
+            new_db.create_postgresql_db()
 
-        # Create new role:
-        admins_role = DatabaseRole('admins', db.connection)
-        admins_role.create_new_role()
+            # Create new role:
+            admins_role = DatabaseRole('admins', db.connection)
+            admins_role.create_new_role()
 
-        # Privilege setting:
-        admins_role_privileges = DatabasePrivilege(new_db.db_name, admins_role.role_name, db.connection)
-        admins_role_privileges.grant_all_privileges()
+            # Privilege setting:
+            admins_role_privileges = DatabasePrivilege(new_db.db_name, admins_role.role_name, db.connection)
+            admins_role_privileges.grant_all_privileges()
 
-        # Create new user:
-        admin_user = DatabaseUser('admin', "1111", db.connection)
-        admin_user.create_new_user()
+            # Create new user:
+            admin_user = DatabaseUser('admin', "1111", db.connection)
+            admin_user.create_new_user()
 
-        # Join user to role:
-        admins_role.join_user_to_role(admin_user.username)
+            # Join user to role:
+            admins_role.join_user_to_role(admin_user.username)
+
+    except DatabaseError as db_error:
+        print(db_error)
+    except Exception as error:
+        print(error)
+
