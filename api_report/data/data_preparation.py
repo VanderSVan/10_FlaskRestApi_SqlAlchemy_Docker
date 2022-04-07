c435de93a7126b39395ce4417b98a388d9c7a53d
VanderSVan <101255612+VanderSVan@users.noreply.github.com>
2022-04-07 18:18:52 +0300

    Add data directory

---
+++ api_report/data/data_preparation.py
@@ -0,0 +1,81 @@
+from string import ascii_uppercase
+from random import choices, sample, choice, randint
+from api_report.models.models import StudentModel, GroupModel, CourseModel
+
+
+def generate_group_name(letters: str, separator: str) -> str:
+    """Creates random string like type: AA-11"""
+    result_list = choices(letters, k=2)
+    two_digit_number = randint(10, 100)
+    result_list.append(separator)
+    result_list.append(str(two_digit_number))
+    return "".join(result_list)
+
+
+def generate_available_places_in_groups(lower_limit_of_students: int,
+                                        upper_limit_of_students: int,
+                                        number_of_groups: int) -> dict:
+    """
+    Creates dictionary with available places as random value and group_id as key.
+    :param lower_limit_of_students: Lower boundary of students in a group.
+    :param upper_limit_of_students: Upper boundary of students in a group.
+    :param number_of_groups: How many students per group.
+    """
+    return {group_id: randint(lower_limit_of_students, upper_limit_of_students)
+            for group_id in range(1, number_of_groups + 1)}
+
+
+def assign_student_to_group(groups: dict) -> int or None:
+    """
+    If there is a place in the group, assigns a student there.
+    While the number of available places will decrease by 1.
+    If there are no available places, returns None.
+    """
+    if sum(groups.values()) == 0:
+        return None
+    for group_id, available_places in groups.items():
+        if available_places == 0:
+            continue
+        groups[group_id] -= 1
+        return group_id
+
+
+def generate_group_instances(number_of_instance: int) -> list:
+    """
+    Type: AA-11
+    Generates random group instances.
+    """
+    return [GroupModel(group_id=group_id,
+                       name=generate_group_name(ascii_uppercase, '-'))
+            for group_id in range(1, number_of_instance + 1)]
+
+
+def generate_course_instances(courses: dict) -> list:
+    """
+    Generates course instances from dict,
+    where course name as key and description as value.
+    """
+    return [CourseModel(course_id=course_id,
+                        name=course[0],
+                        description=course[1])
+            for course_id, course in enumerate(courses.items(), start=1)]
+
+
+def generate_student_instances(number_of_students: int,
+                               first_names: list,
+                               last_names: list,
+                               groups: dict,
+                               courses: CourseModel) -> list:
+    """ Generates random student instances with all relationships. """
+    random_students_list = sample(range(1, number_of_students + 1), number_of_students)
+    return [StudentModel(student_id=student_id,
+                         first_name=choice(first_names),
+                         last_name=choice(last_names),
+                         group_id=assign_student_to_group(groups),
+                         courses=sample(courses, randint(1, 3)))
+            for student_id in random_students_list]
+
+
+if __name__ == '__main__':
+    group_ids = generate_available_places_in_groups(10, 30, 10)
+    print(group_ids)
---
+++ api_report/data/input_data.py
@@ -0,0 +1,33 @@
+
+courses_dict = {'Mathematics': "Mathematics is the science and study of quality, structure, space, and change.",
+                'Chemistry': "Walter H. White : Chemistry is, well technically, chemistry is the study of matter. "
+                             "But I prefer to see it as the study of change.",
+                'Physics': "Physics is the natural science that studies matter, its fundamental constituents, "
+                           "its motion and behavior through space and time, "
+                           "and the related entities of energy and force.",
+                'Electrical engineering': "Electrical engineering is an engineering discipline concerned with "
+                                          "the study, design, and application of equipment, devices, "
+                                          "and systems which use electricity, electronics, and electromagnetism.",
+                'Computer Science': "Computer science is the study of computers and computing as well "
+                                    "as their theoretical and practical applications.",
+                'English': "English is a study of literature, media and language in which students critically "
+                           "and creatively engage with a variety of texts in all language modes.",
+                'Philosophy': "Philosophy  is the study of general and fundamental questions, "
+                              "such as those about existence, reason, knowledge, values, mind, and language.",
+                'Biology': "Biology is a branch of science that deals with living organisms "
+                           "and their vital processes.",
+                'Geometry': "Geometry is concerned with properties of space that are related with distance, "
+                            "shape, size, and relative position of figures.",
+                'Algebra': "Algebra is the study of mathematical symbols and the rules for "
+                           "manipulating these symbols in formulas "
+                           "it is a unifying thread of almost all of mathematics."}
+
+first_names_list = ['JAMES', 'JOHN', 'ROBERT', 'MICHAEL', 'WILLIAM', 'DAVID',
+                    'RICHARD', 'CHARLES', 'JOSEPH', 'THOMAS', 'MARY', 'PATRICIA',
+                    'LINDA', 'BARBARA', 'ELIZABETH', 'JENNIFER', 'MARIA', 'SUSAN',
+                    'MARGARET', 'DOROTHY']
+
+last_names_list = ['JONES', 'SMITH', 'JOHNSON', 'WILLIAMS', 'BROWN', 'DAVIS',
+                   'MILLER', 'MOORE', 'TAYLOR', 'ANDERSON', 'THOMAS', 'JACKSON',
+                   'WHITE', 'HARRIS', 'MARTIN', 'THOMPSON', 'GARCIA', 'MARTINEZ',
+                   'ROBINSON', 'CLARK']
