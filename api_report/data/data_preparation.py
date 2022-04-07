4f5de65c5d55f8367587226bab236ef031548dcd
VanderSVan <101255612+VanderSVan@users.noreply.github.com>
2022-04-07 18:57:17 +0300

    extend 'if __name__' block

--- api_report/data/data_preparation.py
+++ api_report/data/data_preparation.py
@@ -3,7 +3,7 @@ from random import choices, sample, choice, randint
 from api_report.models.models import StudentModel, GroupModel, CourseModel
 
 
-def generate_group_name(letters: str, separator: str) -> str:
+def _generate_group_name(letters: str, separator: str) -> str:
     """Creates random string like type: AA-11"""
     result_list = choices(letters, k=2)
     two_digit_number = randint(10, 100)
@@ -25,7 +25,7 @@ def generate_available_places_in_groups(lower_limit_of_students: int,
             for group_id in range(1, number_of_groups + 1)}
 
 
-def assign_student_to_group(groups: dict) -> int or None:
+def _assign_student_to_group(groups: dict) -> int or None:
     """
     If there is a place in the group, assigns a student there.
     While the number of available places will decrease by 1.
@@ -46,7 +46,7 @@ def generate_group_instances(number_of_instance: int) -> list:
     Generates random group instances.
     """
     return [GroupModel(group_id=group_id,
-                       name=generate_group_name(ascii_uppercase, '-'))
+                       name=_generate_group_name(ascii_uppercase, '-'))
             for group_id in range(1, number_of_instance + 1)]
 
 
@@ -65,17 +65,46 @@ def generate_student_instances(number_of_students: int,
                                first_names: list,
                                last_names: list,
                                groups: dict,
-                               courses: CourseModel) -> list:
+                               courses: list) -> list:
     """ Generates random student instances with all relationships. """
     random_students_list = sample(range(1, number_of_students + 1), number_of_students)
     return [StudentModel(student_id=student_id,
                          first_name=choice(first_names),
                          last_name=choice(last_names),
-                         group_id=assign_student_to_group(groups),
+                         group_id=_assign_student_to_group(groups),
                          courses=sample(courses, randint(1, 3)))
             for student_id in random_students_list]
 
 
 if __name__ == '__main__':
-    group_ids = generate_available_places_in_groups(10, 30, 10)
-    print(group_ids)
+    from input_data import courses_dict, first_names_list, last_names_list
+
+    # Constants
+    test_number_of_groups = 10
+    test_number_of_students = 10
+
+    # Create list of group objects
+    all_groups = generate_group_instances(number_of_instance=test_number_of_groups)
+
+    # Create list of course objects
+    all_courses = generate_course_instances(courses=courses_dict)
+
+    # Create dict with available places as value and group_id as key
+    group_ids = generate_available_places_in_groups(lower_limit_of_students=5,
+                                                    upper_limit_of_students=10,
+                                                    number_of_groups=test_number_of_groups)
+    # Create list of student objects
+    all_students = generate_student_instances(number_of_students=test_number_of_students,
+                                              first_names=first_names_list,
+                                              last_names=last_names_list,
+                                              groups=group_ids,
+                                              courses=all_courses)
+
+    limit_output = 5
+    all_students.sort(key=lambda inst: inst.student_id)
+
+    for student_num, student in enumerate(all_students[:limit_output], start=1):
+        print(student_num)
+        print('full name =', student.first_name, student.last_name, ';')
+        print('in the group:', all_groups[student.group_id - 1].name, ';')
+        print('on courses:', list(map(lambda inst: inst.name, student.courses.all())), '\n')
