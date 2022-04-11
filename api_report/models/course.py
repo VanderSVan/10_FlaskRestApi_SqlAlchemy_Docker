9aea1c62178a4f25f0917f9ef75cfbb473927d7f
VanderSVan <101255612+VanderSVan@users.noreply.github.com>
2022-04-11 19:12:27 +0300

    Add get_all_courses method

--- api_report/models/course.py
+++ api_report/models/course.py
@@ -1,3 +1,4 @@
+from sqlalchemy import asc
 from api_report.db.db_sqlalchemy import db
 
 
@@ -5,5 +6,12 @@ class CourseModel(db.Model):
     __tablename__ = "courses"
 
     course_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String(250), nullable=False)
     description = db.Column(db.Text)
+    students = db.relationship('StudentModel', secondary='students_courses',
+                               back_populates="courses", overlaps="all_students",
+                               order_by='StudentModel.student_id')
+
+    @classmethod
+    def get_all_courses(cls):
+        return cls.query.order_by(asc(cls.course_id))
