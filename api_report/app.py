4afa4becea8f9ce16c8af31b01f6e2ba52f92050
VanderSVan <101255612+VanderSVan@users.noreply.github.com>
2022-04-11 19:20:05 +0300

    Add resources

--- api_report/app.py
+++ api_report/app.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_restful import Api
 from api_report.db.data_insertion_into_db import insert_data_to_db
+from api_report.resources.student import Student, Students, FullStudentStat, FullStudentsStat
+from api_report.resources.course import Courses
+from api_report.resources.group import Groups, SearchGroup
 
 
 def create_app(test_config=False):
@@ -7,6 +11,8 @@ def create_app(test_config=False):
 
     application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     application.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://admin:1111@localhost:5432/university"
+    application.config['JSON_SORT_KEYS'] = False
+    api = Api(application)
 
     @application.before_first_request
     def create_tables():
@@ -16,16 +22,36 @@ def create_app(test_config=False):
 
         db.session.commit()
         print('Tables has been created')
-
     if test_config:
         application.config['TESTING'] = True
+
+    # RESOURCES:
+    # Student
+    api.add_resource(Students, "/students")
+    api.add_resource(Student, "/students/<int:student_id>")
+    api.add_resource(FullStudentsStat, "/students/stat")
+    api.add_resource(FullStudentStat, "/students/stat/<int:student_id>")
+
+    # Courses
+    api.add_resource(Courses, "/courses")
+
+    # Groups
+    api.add_resource(Groups, "/groups")
+    api.add_resource(SearchGroup, "/groups/<int:student_count>")
     return application
 
 
 if __name__ == '__main__':
     from api_report.db.db_sqlalchemy import db
+    from api_report.ma import ma
 
     app = create_app()
     db.init_app(app)
+    ma.init_app(app)
+
+    # with app.app_context():
+    #     print(StudentModel.query.first())
+    #     print(StudentModel.find_by_id(12))
     app.run(debug=True)
+
 
