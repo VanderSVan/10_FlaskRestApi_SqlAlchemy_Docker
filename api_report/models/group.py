9a23663ef6125bb21c10c5f150c35d4084e774da
VanderSVan <101255612+VanderSVan@users.noreply.github.com>
2022-04-11 19:13:03 +0300

    Add get_all_groups method

--- api_report/models/group.py
+++ api_report/models/group.py
@@ -1,3 +1,4 @@
+from sqlalchemy import asc
 from api_report.db.db_sqlalchemy import db
 
 
@@ -6,3 +7,14 @@ class GroupModel(db.Model):
 
     group_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    students = db.relationship('StudentModel', back_populates="group",
+                               order_by='StudentModel.student_id')
+
+    @classmethod
+    def get_all_groups(cls):
+        return cls.query.order_by(asc(cls.group_id))
+
+    # @classmethod
+    # def get_groups_filter_by_student_count(cls, student_count: int):
+    #     print(cls.query.filter(student_count == cls.students.count()))
+    #     return cls.query.filter(student_count == cls.query.filter(cls.students).count())
