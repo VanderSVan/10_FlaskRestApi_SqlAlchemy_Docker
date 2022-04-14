801663a4eeb0d86c5778dbb0b69a9b7daa44f2b4
VanderSVan <101255612+VanderSVan@users.noreply.github.com>
2022-04-14 19:00:43 +0300

    Add delete method StudentList class and improve responses of StudentList class

--- api_report/resources/student.py
+++ api_report/resources/student.py
@@ -1,8 +1,11 @@
 from flask import request, make_response, jsonify
+from flask.wrappers import Response
 from flask_restful import Resource
 from api_report.models.student import StudentModel
 from api_report.schemas.student import ShortStudentSchema, FullStudentSchema
 from api_report.sqlalchemy_queries.queries import ComplexQueries
+from api_report.resources.utils import _convert_string_to_int_list
+from api_report.resources.utils import StudentListResponse
 
 short_student_schema = ShortStudentSchema()
 short_students_schema = ShortStudentSchema(many=True)
@@ -29,7 +32,8 @@ class Student(Resource):
         if not student:
             return make_response(jsonify({'status': 404, 'message': f"student '{student_id}' not found"}), 404)
         student.delete_from_db()
-        return make_response(jsonify({'status': 200, 'message': f"student '{student_id}' was successfully deleted"}), 200)
+        return make_response(jsonify({'status': 200, 'message': f"student '{student_id}' was successfully deleted"}),
+                             200)
 
 
 class StudentList(Resource):
@@ -53,8 +57,30 @@ class StudentList(Resource):
         max_student_id, = StudentModel.get_max_student_id()
         student_list_json = request.get_json()  # student list
         student_objs = full_students_schema.load(student_list_json)
+        added_students = []
         for student in student_objs:
             student.student_id = max_student_id + 1
+            added_students.append(student.student_id)
             max_student_id += 1
             student.save_to_db()
-        return make_response(jsonify({'status': 200, 'message': 'students were successfully added'}), 200)
+        return StudentListResponse.create_response_for_post_method(added_students)
+
+    @classmethod
+    def delete(cls):
+        """file: api_report/Swagger/StudentList/post.yml"""
+        student_ids = request.args.get('student_ids')
+        clean_student_id_list = _convert_string_to_int_list(student_ids, ',')
+        if type(clean_student_id_list) is Response:
+            return make_response(clean_student_id_list, 400)
+
+        nonexistent_students = []
+        deleted_students = []
+        for student_id in clean_student_id_list:
+            student = StudentModel.find_by_id(student_id)
+            if not student:
+                nonexistent_students.append(student_id)
+            else:
+                student.delete_from_db()
+                deleted_students.append(student_id)
+
+        return StudentListResponse.create_response_for_delete_method(deleted_students, nonexistent_students)
