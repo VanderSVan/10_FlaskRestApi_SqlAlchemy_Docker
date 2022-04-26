5daaf78764a2cc60a6bde05bf16f6747bef386dd
VanderSVan <101255612+VanderSVan@users.noreply.github.com>
2022-04-26 14:03:45 +0300

    update config

--- api_university/config.py
+++ api_university/config.py
@@ -3,12 +3,22 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Configuration:
+    DATABASE = {
+        'role_name': 'admins',
+        'user_name': 'admin',
+        'user_password': '1111',
+        'db_name': 'university'
+    }
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://admin:1111@localhost:5432/university"
+    SQLALCHEMY_DATABASE_URI = (f"postgresql+psycopg2://"
+                               f"{DATABASE['user_name']}:"
+                               f"{DATABASE['user_password']}@"
+                               f"localhost:5432/"
+                               f"{DATABASE['db_name']}")
     JSON_SORT_KEYS = False
     API_URL = "/api/v1"
     SWAGGER = {
