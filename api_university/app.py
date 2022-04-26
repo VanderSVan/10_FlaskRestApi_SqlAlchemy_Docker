b28c9cc72bd6d829f695dc6ebeac606424390c8e
VanderSVan <101255612+VanderSVan@users.noreply.github.com>
2022-04-26 13:51:28 +0300

    add config

--- api_university/app.py
+++ api_university/app.py
@@ -4,37 +4,47 @@ from flask_restful import Api
 from flasgger import Swagger
 from marshmallow import ValidationError
 from sqlalchemy.exc import IntegrityError
+
+from api_university.db.db_sqlalchemy import db
+from api_university.ma import ma
+from api_university.config import Configuration, DevelopmentConfiguration, TestingConfiguration
 from api_university.data.data_insertion_into_db import insert_data_to_db
 from api_university.resources.student import Student, StudentList
 from api_university.resources.course import Course, CourseList
 from api_university.resources.group import Group, GroupList
 
 
-def create_app(test_config=False):
+def create_app(test_config=False, dev_config=False):
     application = Flask(__name__)
 
-    application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    application.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://admin:1111@localhost:5432/university"
-    application.config['JSON_SORT_KEYS'] = False
-    application.config['SWAGGER'] = {
-        'doc_dir': './Swagger'
-    }
+    # Config
+    if test_config:
+        application.config.from_object(TestingConfiguration)
+    elif dev_config:
+        application.config.from_object(DevelopmentConfiguration)
+    else:
+        application.config.from_object(Configuration)
+
     api = Api(application)
+    api_url = Configuration.API_URL
     Swagger(
         application,
         template_file=os.path.join('Swagger', 'template.yml'),
         parse=True
     )
 
+    # create db if not exists
     @application.before_first_request
     def create_tables():
         db.create_all()
-
-        insert_data_to_db(db)
-
+        insert_data_to_db(db,
+                          group_count=10,
+                          student_count=200,
+                          lower_limit_students_in_group=10,
+                          upper_limit_of_students_in_group=30)
         db.session.commit()
-        # print('Tables has been created')
 
+    # Handlers
     @application.errorhandler(ValidationError)
     def handle_marshmallow_validation(err):
         print('Got ValidationError =', err)
@@ -50,35 +60,23 @@ def create_app(test_config=False):
         print('Got AttributeError =', err)
         return jsonify(err.args), 400
 
-    if test_config:
-        application.config['TESTING'] = True
-
     # RESOURCES:
     # Student
-    api.add_resource(StudentList, "/students")
-    api.add_resource(Student, "/students/<int:student_id>")
+    api.add_resource(StudentList, f"{api_url}/students")
+    api.add_resource(Student, f"{api_url}/students/<int:student_id>")
 
     # Courses
-    api.add_resource(CourseList, "/courses")
-    api.add_resource(Course, "/courses/<int:course_id>")
+    api.add_resource(CourseList, f"{api_url}/courses")
+    api.add_resource(Course, f"{api_url}/courses/<int:course_id>")
 
     # Groups
-    api.add_resource(GroupList, "/groups")
-    api.add_resource(Group, "/groups/<int:group_id>")
+    api.add_resource(GroupList, f"{api_url}/groups")
+    api.add_resource(Group, f"{api_url}/groups/<int:group_id>")
     return application
 
 
 if __name__ == '__main__':
-    from api_university.db.db_sqlalchemy import db
-    from api_university.ma import ma
-
-    app = create_app()
+    app = create_app(dev_config=True)
     db.init_app(app)
     ma.init_app(app)
-
-    # with app.app_context():
-    #     print(StudentModel.query.first())
-    #     print(StudentModel.find_by_id(12))
-    app.run(debug=True)
-
-
+    app.run()
--- api_university/config.py
+++ api_university/config.py
@@ -1,19 +1,26 @@
-# import os
-# basedir = os.path.abspath(os.path.dirname(__file__))
-#
-#
-# class Config(object):
-#     DEBUG = False
-#     TESTING = False
-#     CSRF_ENABLED = True
-#     SECRET_KEY = 'this-really-needs-to-be-changed'
-#     # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-#
-#
-# class DevelopmentConfig(Config):
-#     DEVELOPMENT = True
-#     DEBUG = True
-#
-#
-# class TestingConfig(Config):
-#     TESTING = True
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Configuration:
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://admin:1111@localhost:5432/university"
+    JSON_SORT_KEYS = False
+    API_URL = "/api/v1"
+    SWAGGER = {
+        'doc_dir': './Swagger',
+        "specs_route": API_URL
+    }
+
+
+class DevelopmentConfiguration(Configuration):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfiguration(Configuration):
+    TESTING = True
