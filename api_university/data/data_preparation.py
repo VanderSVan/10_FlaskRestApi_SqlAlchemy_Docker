98754e1d1cc98c8da385ac6a717c0b169e1c48aa
VanderSVan <101255612+VanderSVan@users.noreply.github.com>
2022-05-27 15:54:13 +0300

    Bugs fix in data_preparation.py, delete conftest1.py, conftest2.py

--- api_university/data/data_preparation.py
+++ api_university/data/data_preparation.py
@@ -8,7 +8,7 @@ from api_university.models.course import CourseModel
 
 def generate_available_places_in_groups(lower_limit_of_students: int,
                                         upper_limit_of_students: int,
-                                        number_of_groups: int) -> dict:
+                                        number_of_groups: int) -> dict[int:int]:
     """
     Creates dictionary with available places as random value and group_id as key.
     :param lower_limit_of_students: Lower boundary of students in a group.
@@ -29,17 +29,17 @@ def _generate_group_name(letters: str, separator: str) -> str:
     return "".join(result_list)
 
 
-def generate_group_instances(number_of_instance: int) -> list:
+def generate_group_instances(number_of_groups: int) -> list[GroupModel]:
     """
     Model: AA-11
     Generates random group instances.
     """
     return [GroupModel(group_id=group_id,
                        name=_generate_group_name(ascii_uppercase, '-'))
-            for group_id in range(1, number_of_instance + 1)]
+            for group_id in range(1, number_of_groups + 1)]
 
 
-def generate_course_instances(courses: dict) -> list:
+def generate_course_instances(courses: dict) -> list[CourseModel]:
     """
     Generates course instances from dict,
     where course name as key and description as value.
@@ -50,32 +50,32 @@ def generate_course_instances(courses: dict) -> list:
             for course_id, course in enumerate(courses.items(), start=1)]
 
 
-def _assign_student_to_group(groups: dict) -> int or None:
+def _assign_student_to_group(available_places_in_groups: dict) -> int or None:
     """
-    If there is a place in the group, assigns a student there.
-    While the number of available places will decrease by 1.
+    If there is a place in the group, assigns a student there,
+    and the number of available places will decrease by 1.
     If there are no available places, returns None.
     """
-    if sum(groups.values()) == 0:
+    if sum(available_places_in_groups.values()) == 0:
         return None
-    for group_id, available_places in groups.items():
+    for group_id, available_places in available_places_in_groups.items():
         if available_places == 0:
             continue
-        groups[group_id] -= 1
+        available_places_in_groups[group_id] -= 1
         return group_id
 
 
 def generate_student_instances(number_of_students: int,
                                first_names: list,
                                last_names: list,
-                               groups: dict,
-                               courses: list) -> list:
+                               available_places_in_groups: dict,
+                               courses: list[CourseModel]) -> list[StudentModel]:
     """ Generates random student instances with all relationships. """
     random_students_list = sample(range(1, number_of_students + 1), number_of_students)
     return [StudentModel(student_id=student_id,
                          first_name=choice(first_names).title(),
                          last_name=choice(last_names).title(),
-                         group_id=_assign_student_to_group(groups),
+                         group_id=_assign_student_to_group(available_places_in_groups),
                          courses=sample(courses, randint(1, 3)))
             for student_id in random_students_list]
 
@@ -89,20 +89,20 @@ if __name__ == '__main__':
     limit_output = 5
 
     # Create list of group objects
-    all_groups = generate_group_instances(number_of_instance=test_number_of_groups)
+    all_groups = generate_group_instances(number_of_groups=test_number_of_groups)
 
     # Create list of course objects
     all_courses = generate_course_instances(courses=courses_dict)
 
     # Create dict with available places as value and group_id as key
-    group_ids = generate_available_places_in_groups(lower_limit_of_students=5,
-                                                    upper_limit_of_students=10,
-                                                    number_of_groups=test_number_of_groups)
+    available_places_for_groups = generate_available_places_in_groups(lower_limit_of_students=5,
+                                                                      upper_limit_of_students=10,
+                                                                      number_of_groups=test_number_of_groups)
     # Create list of student objects
     all_students = generate_student_instances(number_of_students=test_number_of_students,
                                               first_names=first_names_list,
                                               last_names=last_names_list,
-                                              groups=group_ids,
+                                              available_places_in_groups=available_places_for_groups,
                                               courses=all_courses)
 
     all_students.sort(key=lambda inst: inst.student_id)
